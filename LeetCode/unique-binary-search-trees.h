52f0822c25e0f9517a57afc590b832f7136647a1
edwlin <keamoulin@yahoo.com>
2014-03-11 10:13:16 +0800

    Create unique-binary-search-trees.h

---
+++ LeetCode/unique-binary-search-trees.h
@@ -0,0 +1,30 @@
+/*
+Given n, how many structurally unique BST's (binary search trees) that store values 1...n?
+
+For example,
+Given n = 3, there are a total of 5 unique BST's.
+
+   1         3     3      2      1
+    \       /     /      / \      \
+     3     2     1      1   3      2
+    /     /       \                 \
+   2     1         2                 3
+*/
+
+/////////////////////////////////////////////////////////////////////////////////////////
+
+class Solution {
+public:
+    int numTrees(int n) {
+        if ( n <= 1 ) return n;
+        vector<int> nums( n+1, 1 );
+        for ( int i=2; i<=n; i++ ) {
+            int tmp = nums[i-1] * 2;
+            for ( int j=2; j<i; j++ ) {
+                tmp += nums[j-1] * nums[ i-j ];
+            }
+            nums[i] = tmp;
+        }
+        return nums[n];
+    }
+};
