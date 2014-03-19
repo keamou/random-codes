ac86adc2e8723dc651e6c6ab5ab592189d08332a
edwlin <keamoulin@yahoo.com>
2014-03-19 16:55:48 +0800

    Create best-time-to-buy-and-sell-stock-iii.h

---
+++ LeetCode/best-time-to-buy-and-sell-stock-iii.h
@@ -0,0 +1,42 @@
+/*
+Say you have an array for which the ith element is the price of a given stock on day i.
+
+Design an algorithm to find the maximum profit. You may complete at most two transactions.
+
+Note:
+You may not engage in multiple transactions at the same time (ie, you must sell the stock before you buy again).
+*/
+
+////////////////////////////////////////////////////////////////////
+
+class Solution {
+public:
+    int maxProfit(vector<int> &prices) {
+        if ( prices.empty() ) return 0;
+        vector<int> maxvec( prices.size() );
+        vector<int> minvec( prices.size() );
+        int maxval = prices.back(), minval = prices.front(), result = 0, tmp;
+        for ( int i=prices.size()-1; i>=0; i-- ) {
+            maxval = (maxval < prices[i]) ? prices[i] : maxval;
+            maxvec[i] = maxval - prices[i];
+        }
+        maxval = 0;
+        for ( int i=prices.size()-1; i>=0; i-- ) {
+            maxvec[i] = maxval = (maxval < maxvec[i]) ? maxvec[i] : maxval;
+        }
+        for ( int i=0; i<prices.size(); i++ ) {
+            minvec[i] = minval = (minval > prices[i]) ? prices[i] : minval;
+            minvec[i] = prices[i] - minval;
+        }
+        minval = 0;
+        for ( int i=0; i<prices.size(); i++ ) {
+            minvec[i] = minval = (minval < minvec[i]) ? minvec[i] : minval;
+        }
+        for ( int i=0; i<prices.size(); i++ ) {
+            //tmp = getFirst(prices, minvec, i) + getSecond(prices, maxvec, i);
+            tmp = maxvec[i] + minvec[i];
+            if ( tmp > result ) result = tmp;
+        }
+        return result;
+    }
+};
