5075742d9ad7116878e32b753b4c614a99a545e7
edwlin <keamoulin@yahoo.com>
2014-03-19 13:35:42 +0800

    Create best-time-to-buy-and-sell-stock-ii.h

---
+++ LeetCode/best-time-to-buy-and-sell-stock-ii.h
@@ -0,0 +1,24 @@
+/*
+Say you have an array for which the ith element is the price of a given stock on day i.
+
+Design an algorithm to find the maximum profit. You may complete as many transactions as you like (ie, buy one and sell one share of the stock multiple times). However, you may not engage in multiple transactions at the same time (ie, you must sell the stock before you buy again).
+*/
+
+///////////////////////////////////////////////////////////////////
+
+class Solution {
+public:
+    int maxProfit(vector<int> &prices) {
+        if ( prices.empty() ) return 0;
+        int maxval = prices.back(), result = 0;
+        for ( int i=prices.size()-1; i>=0; i-- ) {
+            if ( prices[i] < maxval ) {
+                result += maxval - prices[i];
+                maxval = prices[i];
+            } else {
+                maxval = prices[i];
+            }
+        }
+        return result;
+    }
+};
