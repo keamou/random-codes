1e6a1d7471bbfb8050908b6186c31ad8385f45ba
edwlin <keamoulin@yahoo.com>
2014-03-25 16:26:57 +0800

    Create unique-paths-ii.h

---
+++ LeetCode/unique-paths-ii.h
@@ -0,0 +1,53 @@
+/*
+Follow up for "Unique Paths":
+
+Now consider if some obstacles are added to the grids. How many unique paths would there be?
+
+An obstacle and empty space is marked as 1 and 0 respectively in the grid.
+
+For example,
+There is one obstacle in the middle of a 3x3 grid as illustrated below.
+
+[
+  [0,0,0],
+  [0,1,0],
+  [0,0,0]
+]
+The total number of unique paths is 2.
+
+Note: m and n will be at most 100.
+*/
+
+///////////////////////////////////////////////////////////////////
+
+
+class Solution {
+public:
+    int uniquePathsWithObstacles(vector<vector<int> > &obstacleGrid) {
+        int m = obstacleGrid.size();
+        if ( m == 0 ) return 0;
+        int n = obstacleGrid[0].size();
+        if ( n == 0 ) return 0;
+        vector< vector<int> > results( m, vector<int>(n, 0) );
+        if ( obstacleGrid[0][0] == 1 ) return 0;
+        results[0][0] = 1;
+        for ( int i=0; i<m; i++ ) for (int j=0; j<n; j++ ) {
+            if ( obstacleGrid[i][j] == 1 ) continue;
+            if ( i == 0 ) {
+                if ( j > 0 ) { 
+                    if (obstacleGrid[i][j-1] == 1 )  break;
+                    results[i][j] = results[i][j-1];
+                }
+            } else {
+                if ( j == 0 ) {
+                    if ( obstacleGrid[i-1][j] == 1 ) continue;
+                    results[i][j] = results[i-1][j];
+                } else {
+                    if ( obstacleGrid[i-1][j] != 1 ) results[i][j] += results[i-1][j];
+                    if ( obstacleGrid[i][j-1] != 1 ) results[i][j] += results[i][j-1];
+                }
+            }
+        }
+        return results.back().back();
+    }
+};
